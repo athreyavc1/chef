590f36b85cd618c41a388bb4f66c84ee15f7b2f5
madhu <madhu@MacBook-Pro.home>
2017-07-02 11:28:44 +0100

    Corrected few Errors

--- mytestsite/recipes/default.rb
+++ mytestsite/recipes/default.rb
@@ -18,14 +18,13 @@ service 'apache2' do
   action :enable
 end
 
-<<<<<<< HEAD
+
 remote_file  "/opt/#{node['home']['projectname']}.zip" do
   source  "http://192.168.1.71/data/artifacts/lacegant.zip"
-=======
+end
 
 artifact_file  "/opt/#{node['home']['projectname']}.zip" do
   location  "http://www.free-css.com/assets/files/free-css-templates/download/page211/lacegant.zip"
->>>>>>> 7a9f626a8526a2d317df3de5255e19b34a4b9fe0
   owner "root"
   group "root"
   action :create
@@ -37,13 +36,8 @@ web_app "#{node['home']['projectname']}" do
 end
 
 
-<<<<<<< HEAD
+
 zipfile "/opt/#{node['home']['projectname']}.zip" do
 	into "#{node['home']['webroot']}"
 	not_if {  ::Dir.exist?("#{node['home']['webroot']}/#{node['home']['projectname']}")}
-=======
-zipfile "/opt/#{node['home']['projectname']}.zip" do 
-	into "#{node['home']['webroot'] }"
-	not_if { File.exists?("#{node['home']['webroot']}/#{node['home']['projectname']}")}
->>>>>>> 7a9f626a8526a2d317df3de5255e19b34a4b9fe0
 end
