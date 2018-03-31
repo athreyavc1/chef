d38159c10ab6a956b1c56d89536331fc96b56020
jnananwesha <jnananwesha@mail.com>
2018-03-31 11:47:45 +0100

    Testing

--- homelab-openvpn/recipes/default.rb
+++ homelab-openvpn/recipes/default.rb
@@ -5,14 +5,16 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 node.default['openvpn']['port'] = "1194"
-#node.default['openvpn']['user'] = "vagrant"
-#node.default['openvpn']['group'] = "vagrant"
+node.default['openvpn']['user'] = "vagrant"
+node.default['openvpn']['group'] = "vagrant"
 node.default['openvpn']['config_dir'] = "/etc/openvpn"
 include_recipe 'openvpn::install'
 
 node.default['openvpn']['configure_default_server'] = false
 
-node.default['openvpn']['config'] = { "port": node['openvpn']['port'],
+node.default['openvpn']['config'] = {
+          "local": node['ipaddress'],
+          "port": node['openvpn']['port'],
           "server":  "192.168.1.0  255.255.255.0",
           "verb": "3",
           "cert":  "#{node['openvpn']['config_dir']}/server.crt",
@@ -22,7 +24,10 @@ node.default['openvpn']['config'] = { "port": node['openvpn']['port'],
           "user": node['openvpn']['user'],
           "group": node['openvpn']['group'],
           "push":  ["\"dhcp-option DNS 8.8.8.8\"", "\"dhcp-option DNS 8.8.4.4\""],
-          "dev": "tun"
+          "push": ["\'route 192.168.1.0 255.255.255.0\'"],
+          "dev": "tun",
+          "keepalive": "10 120",
+
           }
 
 openvpn_conf 'server' do
@@ -33,8 +38,8 @@ end
   cookbook_file "/etc/openvpn/#{line}" do
     source "#{line}"
     mode 0600
-    owner "vagrant"
-    group "vagrant"
+    owner node['openvpn']['user']
+    group node['openvpn']['group']
     action :create
   end
 end
