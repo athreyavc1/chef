42cfdf7f70fefc6d4c3d69861e42e60686f9465d
jnananwesha <jnananwesha@mail.com>
2018-03-31 10:14:38 +0100

    Testing

--- homelab-openvpn/recipes/default.rb
+++ homelab-openvpn/recipes/default.rb
@@ -5,8 +5,8 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 node.default['openvpn']['port'] = "1194"
-node.defaut['openvpn']['user'] = "vagrant"
-node.defaut['openvpn']['group'] = "vagrant"
+node.default['openvpn']['user'] = "vagrant"
+node.default['openvpn']['group'] = "vagrant"
 node.default['openvpn']['config_dir'] = "/etc/openvpn"
 include_recipe 'openvpn::install'
 
@@ -23,14 +23,14 @@ node.default['openvpn']['config'] = { "port": node['openvpn']['port'],
           "push":  ["\"dhcp-option DNS 8.8.8.8\"", "\"dhcp-option DNS 8.8.4.4\""]
         }
 
-%w(server.crt,dh.pem,server.key).each do | line | 
-  cookbook_file "#{node['openvpn']['config_dir']}/#{line}" do 
+%w(server.crt,dh.pem,server.key).each do | line |
+  cookbook_file "#{node['openvpn']['config_dir']}/#{line}" do
     source "#{line}"
     chmod 0600
     owner node['openvpn']['user']
     group node['openvpn']['group']
-  end 
-end 
+  end
+end
 openvpn_conf 'server' do
   config  node['openvpn']['config']
 end
