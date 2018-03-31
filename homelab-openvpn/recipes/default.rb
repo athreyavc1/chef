cbd7d1d33e0f53046d640c62fa957aabccbddae7
jnananwesha <jnananwesha@mail.com>
2018-03-31 10:21:55 +0100

    testing

--- homelab-openvpn/recipes/default.rb
+++ homelab-openvpn/recipes/default.rb
@@ -4,31 +4,31 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-node.default['openvpn']['port'] = "1194"
-node.default['openvpn']['user'] = "vagrant"
-node.default['openvpn']['group'] = "vagrant"
-node.default['openvpn']['config_dir'] = "/etc/openvpn"
+#node.default['openvpn']['port'] = "1194"
+#node.default['openvpn']['user'] = "vagrant"
+#node.default['openvpn']['group'] = "vagrant"
+#node.default['openvpn']['config_dir'] = "/etc/openvpn"
 include_recipe 'openvpn::install'
 
 node.default['openvpn']['configure_default_server'] = false
-node.default['openvpn']['config'] = { "port": node['openvpn']['port'],
+node.default['openvpn']['config'] = { "port": "1194",
           "server":  "192.168.1.0  255.255.255.0",
-          "proto": "udp"
+          "proto": "udp",
           "verb":  "3",
-          "cert":  "#{node['openvpn']['config_dir']}/server.crt",
-          "key" :  "#{node['openvpn']['config_dir']}/server.key",
-          "dh":    "#{node['openvpn']['config_dir']}/dh.pem",
-          "user":  node['openvpn']['user'],
-          "group": node['openvpn']['group'],
+          "cert":  "/etc/openvpn/server.crt",
+          "key" :  "/etc/openvpn//server.key",
+          "dh":    "/etc/openvpn//dh.pem",
+          "user":  "vagrant",
+          "group": "vagrant",
           "push":  ["\"dhcp-option DNS 8.8.8.8\"", "\"dhcp-option DNS 8.8.4.4\""]
         }
 
 %w(server.crt,dh.pem,server.key).each do | line |
-  cookbook_file "#{node['openvpn']['config_dir']}/#{line}" do
+  cookbook_file "/etc/openvpn/#{line}" do
     source "#{line}"
     chmod 0600
-    owner node['openvpn']['user']
-    group node['openvpn']['group']
+    owner "vagrant"
+    group "vagrant"
   end
 end
 openvpn_conf 'server' do
