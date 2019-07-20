e9218e19224feb11f980e9f90973edd076f5d7e1
jnananwesha <jnananwesha@mail.com>
2019-07-20 16:18:34 +0100

    Adding artifact resource

--- artifacttest/metadata.rb
+++ artifacttest/metadata.rb
@@ -5,7 +5,9 @@ license 'All Rights Reserved'
 description 'Installs/Configures artifacttest'
 long_description 'Installs/Configures artifacttest'
 version '0.1.0'
-chef_version '>= 13.0'
+#chef_version '>= 13.0'
+
+depends 'artifact'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
--- artifacttest/recipes/default.rb
+++ artifacttest/recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+artifact_file  "/opt/test.zip" do
+  location  "http://www.free-css.com/assets/files/free-css-templates/download/page211/lacegant.zip"
+  owner "root"
+  group "root"
+  action :create
+end
